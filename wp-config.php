f6c82670fd004e2770d8be57d3304b64cb984262
Dmitry Nizovtsev <dmitry@ukrteam.com>
2012-02-21 16:03:58 +0200

    add center block on home page

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'hadley_db');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');

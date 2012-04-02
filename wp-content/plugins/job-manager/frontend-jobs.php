bdb21b63c4c2a702e9b0bfcf4fac6b9a24e05cbf
Ilya Nechiporenko <ilya.nechiporenko@gmail.com>
2012-04-02 16:00:39 +0300

    comments

--- wp-content/plugins/job-manager/frontend-jobs.php
+++ wp-content/plugins/job-manager/frontend-jobs.php
@@ -108,6 +108,7 @@ function jobman_display_jobs_list( $cat ) {
 	}
 	$related_cats = array_unique( $related_cats );
 
+    // hide short codes if a single category is show
 	if( $options['related_categories'] && count( $related_cats ) > 1 ) {
 		$links = array();
         $i = 0;
